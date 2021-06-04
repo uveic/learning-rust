47e1fba3736fce6b47a2e03603b4edf4c156abee
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-06-04 20:06:37 +0200

    Packages, Crates and Modules

---
+++ 07_restaurant/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+use std::fmt;
+
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+use std::{cmp::Ordering, io};
+
+use std::collections::*;
+
+// fn function1() -> fmt::Result {
+//     // do something
+// }
+//
+// fn function2() -> io::Result<()> {
+//     // do something
+// }
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+}
