0893572ea672f762f9fad472747ff24ce5674132
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-06-09 11:32:22 +0200

    Error handling

--- 02_guessing_game/src/main.rs
+++ 02_guessing_game/src/main.rs
@@ -2,13 +2,35 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut count = 1;
 
     loop {
+        println!("==========================");
+        println!("Attempt: {}", count);
         println!("Please, input your guess: ");
+        count += 1;
 
         let mut guess = String::new();
 
@@ -16,10 +38,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess : u32 = match guess.trim().parse() {
+        let guess : i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100");
+            continue;
+        }
 
         println!("You guessed: {}", guess);
 
