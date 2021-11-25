27c47f2684beb84b4fdd9452442605856941b4d2
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-11-25 18:56:57 +0100

    WIP: Final Project

---
+++ 20_server/src/bin/main.rs
@@ -0,0 +1,49 @@
+use std::{fs, thread};
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        let pool = ThreadPool::new(4);
+
+        pool.execute(|| {
+            handle_connection(stream)
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "public/index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(3));
+        ("HTTP/1.1 200 OK", "public/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "public/404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content,
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
