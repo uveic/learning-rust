a828b4249c65500039af33f3432f028b1581e63c
Victor Gonzalez <victor.gonzalez@protonmail.com>
2021-12-21 12:01:10 +0100

    Advent calendar of code: day 3, part 1

--- advent/src/lib.rs
+++ advent/src/lib.rs
@@ -79,3 +79,47 @@ pub mod day02 {
         println!("Result moving submarine: {}", horizontal * depth);
     }
 }
+
+pub mod day03 {
+    use std::collections::HashMap;
+
+    pub fn calculate_power_consumption(content: &Vec<&str>) -> () {
+        let mut bits: HashMap<(i32, char), i32> = HashMap::new();
+
+        for item in content {
+            for (i, c) in item.chars().enumerate() {
+                let entry: (i32, char) = (i as i32, c);
+                let count = bits.entry(entry).or_insert(0);
+                *count += 1;
+            }
+        }
+
+        let mut max: HashMap<i32, char> = HashMap::new();
+        let mut min: HashMap<i32, char> = HashMap::new();
+        let end: i32 = content[0].len() as i32;
+        for i in 0..end {
+            let zero = bits.get(&(i, '0')).unwrap();
+            let one = bits.get(&(i, '1')).unwrap();
+
+            max.insert(i, if zero > one { '0' } else { '1' });
+            min.insert(i, if zero < one { '0' } else { '1' });
+        }
+
+        let mut max_vec: Vec<(&i32, &char)> = max.iter().collect();
+        max_vec.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let gamma_rate: String = max_vec.iter().map(|x| x.1.to_string()).collect();
+        let gamma_rate_decimal = isize::from_str_radix(&gamma_rate.to_string(), 2).unwrap();
+
+        println!("Gamma rate: {}, {}", gamma_rate, gamma_rate_decimal);
+
+        let mut min_vec: Vec<(&i32, &char)> = min.iter().collect();
+        min_vec.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let epsilon_rate: String = min_vec.iter().map(|x| x.1.to_string()).collect();
+        let epsilon_rate_decimal = isize::from_str_radix(&epsilon_rate.to_string(), 2).unwrap();
+
+        println!("Epsilon rate: {}, {}", epsilon_rate, epsilon_rate_decimal);
+        println!("Power consumption: {}", gamma_rate_decimal * epsilon_rate_decimal);
+    }
+}
--- advent/src/main.rs
+++ advent/src/main.rs
@@ -1,19 +1,25 @@
 use std::fs;
 use advent::day01::{count_individual_measurements, count_three_window};
 use advent::day02::{move_submarine, move_submarine_aim};
+use advent::day03::calculate_power_consumption;
 
 fn main() {
     println!("############ DAY 1 ############");
-    let content = fs::read_to_string(String::from("data/measurements.txt")).unwrap();
+    let content = fs::read_to_string(String::from("data/day01_measurements.txt")).unwrap();
     let input: Vec<i32> = content.split("\n")
         .map(|line| line.parse::<i32>().unwrap_or_else(|_| 0))
         .filter(|&mass| mass > 0).collect();
     count_individual_measurements(&input);
     count_three_window(&input);
 
-    println!("############ DAY 2 ############");
-    let content: String = fs::read_to_string(String::from("data/submarine.txt")).unwrap();
+    println!("\n############ DAY 2 ############");
+    let content: String = fs::read_to_string(String::from("data/day02_submarine.txt")).unwrap();
     let input: Vec<&str> = content.split("\n").filter(|l| l.len() > 0).collect();
     move_submarine(&input);
     move_submarine_aim(&input);
+
+    println!("\n############ DAY 3 ############");
+    let content: String = fs::read_to_string(String::from("data/day03_report.txt")).unwrap();
+    let input: Vec<&str> = content.split("\n").filter(|l| l.len() > 0).collect();
+    calculate_power_consumption(&input);
 }
